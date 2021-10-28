afa4bbb7a4a65689c990cc820b06cbb615830351
MiThCeKi <michaeltcking@me.com>
2021-10-28 09:49:05 -0230

    starting new branch with a hide/show attemp

--- hidden.js
+++ hidden.js
@@ -1,4 +1,10 @@
 
 function hideElementOnClick() {
+    // I know this is against the DRY principle. Can we discuss how to fix this?
+    // I want the click to call one query selector which is associated with one class. 
+    // Is it possible to grab the class name from the menu and insert it in the query call?
     var clickedElement = document.querySelector(.)
+    var clickedElement = document.querySelector(.)
+    var clickedElement = document.querySelector(.)
+
 }
