576367ade0d5206accdde9284e2ed92109ae28bc
MiThCeKi <michaeltcking@me.com>
2021-10-28 10:46:15 -0230

    got the text Main Graph to display but not the whole box. Next I am going to try to replace the text dynamically as I click other nav items.

--- hidden.js
+++ hidden.js
@@ -1,10 +1,7 @@
 
-function hideElementOnClick() {
-    // I know this is against the DRY principle. Can we discuss how to fix this?
-    // I want the click to call one query selector which is associated with one class. 
-    // Is it possible to grab the class name from the menu and insert it in the query call?
-    var clickedElement = document.querySelector(.)
-    var clickedElement = document.querySelector(.)
-    var clickedElement = document.querySelector(.)
-
+function showFunction () {
+  const show = document.getElementById('MainGraph')
+  show.className += 'show_main_graph'
 }
+
+showFunction()
