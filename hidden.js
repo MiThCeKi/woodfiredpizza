0c8dbdbbbd375339da2c2bc930d979da7cce7580
MiThCeKi <michaeltcking@me.com>
2021-10-28 12:50:23 -0230

    can't get Main Graph to hide when showing the Graph Highlights and vice versa

--- hidden.js
+++ hidden.js
@@ -1,6 +1,23 @@
 
-function showFunction () {
-  const show = document.getElementById('MainGraph')
-
-  show.className += 'show_main_graph'
+function showMainGraph () {
+  console.log('indicator for showMainGraph')
+  const showMainGraph = document.getElementById('MainGraph')
+  const hideGraphHighlights = document.getElementById('GraphHighlights')
+  hideGraphHighlights.className += 'hidden'
+  showMainGraph.className += 'show_main_graph'
 }
+
+function showGraphHighlights () {
+  console.log('indicator for showGraphHighlights')
+  const hideMainGraph = document.getElementById('MainGraph')
+  const showGraphHighlights = document.getElementById('GraphHighlights')
+  hideMainGraph.className += 'hidden'
+  showGraphHighlights.className += 'show_GraphHighlights'
+}
+showMainGraph()
+showGraphHighlights()
+
+// ideally, I would have some sort of listen or way to capture
+// what I clicked and then feed that into the ID
+// I'd also have to set up that when this happens to hide the current one.
+// OR I could set up a boolean ?
