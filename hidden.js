8b355b23786d846e7d81c690a4d0d9baac6b8e64
MiThCeKi <michaeltcking@me.com>
2021-10-29 10:17:57 -0230

    making changes during Thursday session for a click-based appear/disappear.

--- hidden.js
+++ hidden.js
@@ -1,22 +1,26 @@
 
-function showMainGraph () {
-  console.log('indicator for showMainGraph')
-  const showMainGraph = document.getElementById('MainGraph')
-  const hideGraphHighlights = document.getElementById('GraphHighlights')
-  hideGraphHighlights.className += 'hidden'
-  showMainGraph.className += 'show_main_graph'
+//const pages = [document.querySelector('#MainGraph'), document.querySelector('#GraphHighlights'), document.querySelector('#EventPlanner')]
+
+//is only executed once DOM is in place. The arrow function declares a function too and exists for reasons unknown at this time. let it be.
+//Function showMainGraph () { } are the same showMainGraph = () => { }
+
+//can declare a variable in global scope to be used. No value. 
+let activePage
+
+//review this code and learn a tiny bit about arrow functions. 
+window.onload = () => {
+  activePage = document.querySelector('#MainGraph')
+  activePage.classList.remove('hidden')
 }
 
-
+//make this more DRY by using event listeners at some point.
 function showGraphHighlights () {
-  console.log('indicator for showGraphHighlights')
-  const hideMainGraph = document.getElementById('MainGraph')
-  const showGraphHighlights = document.getElementById('GraphHighlights')
-  hideMainGraph.className += 'hidden'
-  showGraphHighlights.className += 'show_GraphHighlights'
+  activePage.classList.add('hidden')
+  activePage = document.querySelector('#GraphHighlights')
+  activePage.classList.remove('hidden')
 }
-showMainGraph()
-showGraphHighlights()
+
+
 // to keep DRY, I would have some sort of listen or way to capture
 // what I clicked and then feed that into the ID
 //should I use the ${}?
