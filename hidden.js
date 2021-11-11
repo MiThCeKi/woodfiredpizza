f1026c2795069ea74e8c5bb73cdae9801e373a00
MiThCeKi <michaeltcking@me.com>
2021-11-11 11:17:03 -0330

    adding hidden js file again

--- hidden.js
+++ hidden.js
@@ -1,10 +1,9 @@
 
 //const pages = [document.querySelector('#MainGraph'), document.querySelector('#GraphHighlights'), document.querySelector('#EventPlanner')]
-
 //is only executed once DOM is in place. The arrow function declares a function too and exists for reasons unknown at this time. let it be.
 //Function showMainGraph () { } are the same showMainGraph = () => { }
-
 //can declare a variable in global scope to be used. No value. 
+
 let activePage
 
 //review this code and learn a tiny bit about arrow functions. 
