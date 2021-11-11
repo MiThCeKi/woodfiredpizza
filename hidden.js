e32156c88d214107fc89ccf13635e5d083db614e
MiThCeKi <michaeltcking@me.com>
2021-11-11 16:18:01 -0330

    please just add the hidden.js

--- hidden.js
+++ hidden.js
@@ -1,20 +1,20 @@
 
-//const pages = [document.querySelector('#MainGraph'), document.querySelector('#GraphHighlights'), document.querySelector('#EventPlanner')]
+// const pages = [document.querySelector('#MainGraph'), document.querySelector('#GraphHighlights'), document.querySelector('#EventPlanner')]
 
-//is only executed once DOM is in place. The arrow function declares a function too and exists for reasons unknown at this time. let it be.
-//Function showMainGraph () { } are the same showMainGraph = () => { }
+// is only executed once DOM is in place. The arrow function declares a function too and exists for reasons unknown at this time. let it be.
+// Function showMainGraph () { } are the same showMainGraph = () => { }
 
-//can declare a variable in global scope to be used. No value. 
+//  can declare a variable in global scope to be used. No value. 
 let activePage
 
-//review this code and learn a tiny bit about arrow functions. 
+// review this code and learn a tiny bit about arrow functions. 
 window.onload = () => {
   activePage = document.querySelector('#MainGraph')
   activePage.classList.remove('hidden')
 }
 
-//try to get a console.log printout of something I click on. 
-//make this more DRY by using event listeners at some point.
+// try to get a console.log printout of something I click on. 
+// make this more DRY by using event listeners at some point.
 
 function showGraphHighlights () {
   activePage.classList.add('hidden')
@@ -30,13 +30,10 @@ function showMainGraph () {
 
 window.addEventListener('click', (event) => {
   wantedElement = event.target
-  console.log(wantedElement)
-  console.log(wantedElement.id)
   idOfWantedElement = wantedElement.id
   return idOfWantedElement
 })
 
-
-// function genericShow (){
-//   activePage = document.querySelector('')
-//}
+//  function genericShow (){
+//    activePage = document.querySelector('')
+// }
