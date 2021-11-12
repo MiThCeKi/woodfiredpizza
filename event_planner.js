bc3f713631cf843bf7b5e553031cafe64be64158
MiThCeKi <michaeltcking@me.com>
2021-11-12 14:31:42 -0330

    Working on event_planner.js . Creating an object from form, adding an event listener, e.preventDefault, which prevents reloading of the page, a new FormData  object from that event_planner_form object, then creating the fetch API with a promise that prints the response text and body - I don't know which one to print.

--- event_planner.js
+++ event_planner.js
@@ -2,6 +2,18 @@
 
 const event_planner_form  = document.querySelector('#event_planner_form');
 
-event_planner_form.addEventListener('submit', (e)=>{
+event_planner_form.addEventListener('submit', function (e) =>{
   e.preventDefault()
+  
+  const formData = new FormData(event_planner_form)
+
+  fetch ('<some address>, {
+    method: 'post',
+    body: 'event_planner_form',
+  }).then(function (response) {
+    console.log(response.text)
+    console.log(response.body)
+    return response.text
+  }
+
 })
