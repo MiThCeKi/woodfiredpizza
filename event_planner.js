2c0e49b7c78614529cb8966ba978526cd3cd8205
MiThCeKi <michaeltcking@me.com>
2021-11-14 10:16:53 -0330

    adding a JSON placeholder

--- event_planner.js
+++ event_planner.js
@@ -2,14 +2,14 @@
 
 const event_planner_form  = document.querySelector('#event_planner_form');
 
-event_planner_form.addEventListener('submit', function (e) =>{
+event_planner_form.addEventListener('submit', function (e) {
   e.preventDefault()
   
   const formData = new FormData(event_planner_form)
 
   const jsonPayload = JSON.stringify(Object.fromEntries(formData));
 
-  fetch ('<MOCK-API-PLACEHOLDER>',  {
+  fetch ('https://jsonplaceholder.typicode.com/todos/1', {
       method: 'post',
       body: 'jsonPayload',
   }).then(function (response) {
@@ -17,7 +17,7 @@ event_planner_form.addEventListener('submit', function (e) =>{
     console.log(response.body)
     return response.text;
   }).catch(function (error){
-    console.error(error);
+    console.warn('something is a foot', error);
   })
 
-})
+});
