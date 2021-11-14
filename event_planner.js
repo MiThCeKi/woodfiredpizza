45b252d5770278d2156ad91792f2abe1ae4b415e
MiThCeKi <michaeltcking@me.com>
2021-11-14 10:28:24 -0330

    touching up linting

--- event_planner.js
+++ event_planner.js
@@ -4,20 +4,16 @@ const event_planner_form  = document.querySelector('#event_planner_form');
 
 event_planner_form.addEventListener('submit', function (e) {
   e.preventDefault()
-  
   const formData = new FormData(event_planner_form)
 
   const jsonPayload = JSON.stringify(Object.fromEntries(formData));
 
-  fetch ('https://jsonplaceholder.typicode.com/todos/1', {
-      method: 'post',
-      body: 'jsonPayload',
+  fetch('https://jsonplaceholder.typicode.com/todos/1', {
+    method: 'post',
+    body: 'jsonPayload',
   }).then(function (response) {
-    console.log(response.text)
-    console.log(response.body)
-    return response.text;
+    console.log('its working!', response)
   }).catch(function (error){
-    console.warn('something is a foot', error);
+    console.warn('something is a foot', error)
   })
-
 });
