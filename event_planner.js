ef49e59bf6372a27a4262f5c7a0c5b88833d990c
MiThCeKi <michaeltcking@me.com>
2021-11-17 06:52:14 -0330

    added some questions

--- event_planner.js
+++ event_planner.js
@@ -5,6 +5,7 @@ const event_planner_form  = document.querySelector('#event_planner_form');
 event_planner_form.addEventListener('submit', function (e) {
   e.preventDefault();
   const formData = new FormData(event_planner_form);
+  // the jsonpayload is created from the form data. The form data into an js object that is then put into a JSON string.
   const jsonPayload = JSON.stringify(Object.fromEntries(formData));
   
   fetch('https://6191267b41928b001768ff71.mockapi.io/wfp/pizza',{method: 'post',body: 'jsonPayload',
@@ -19,11 +20,13 @@ event_planner_form.addEventListener('submit', function (e) {
 
       return Promise.reject(response);
     }
+    // in the tutorial on go make things I don't undrestand on how he can put in this (data). Where does it come from?
   }).then(function (data) {
     console.log(data);
   }).catch(function (error){
     console.warn('something is a foot', error)
     // what should I do here after an error is found. Can I perform any preprogrammed steps to handle it and recover?
+    // I'm thinking like if the connection is down for a moment - what error would arrive - and then we put in a wait and then run the function again.
   })
 });
 
