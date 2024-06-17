c526805c078616ff13460bbc9821094189cc7660
MiThCeKi <michaeltcking@me.com>
2024-06-17 18:53:23 -0230

    adding models and figuring out what is JSON serializable into a dataframe

--- pizza_and_bread/pizza_and_bread/main.py
+++ pizza_and_bread/pizza_and_bread/main.py
@@ -1,15 +1,32 @@
-
-
-
 #region importing the required libraries
+import typing
 from fastapi import FastAPI
+import pydantic
+from pizza_and_bread.models import TemperatureRecord, TemperatureResponse
+
+import pandas as pd
 #endregion
 
 
 app  = FastAPI()
 
 #this end point handles a get request. 
-@app.get("/")
-async def print_text(): #defining with async in case I have concurrent requests
-    return {"message": "going to make pizza and bread"}
+@app.get("/pizza/get_temperature", response_model=TemperatureRecord, tags=["pizza_temperature"])
+async def print_text(text)-->pd.DataFrame:
+    """
+    docstring: Defining with async in case I have concurrent requests
+    Returns: a dictionary with a message key and a value of "going to make pizza and bread"
+    """
+    text: str
+    raw_data = TemperatureRecord
 
+    
+    #data = some transformation of TemperatureRecord
+    
+    try:
+        #return the message
+        return data
+    except pydantic.ValidationError as e:
+        #return the error message
+        return {"error": str(e)}
+    
