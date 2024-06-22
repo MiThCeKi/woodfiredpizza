1b0a7466b6a336326ca5d4a6d56100fcd98cdf25
MiThCeKi <michaeltcking@me.com>
2024-06-22 13:21:53 -0230

    saving

--- pizza_and_bread/pizza_and_bread/main.py
+++ pizza_and_bread/pizza_and_bread/main.py
@@ -2,7 +2,7 @@
 import typing
 from fastapi import FastAPI
 import pydantic
-from pizza_and_bread.models import TemperatureRecord, TemperatureResponse
+from pizza_and_bread.models import DTO_TemperatureRecord, TemperatureResponse
 
 import pandas as pd
 #endregion
@@ -10,16 +10,17 @@ import pandas as pd
 
 app  = FastAPI()
 
-#this end point handles a get request. 
-@app.get("/pizza/get_temperature", response_model=DTO_TemperatureRecord, tags=["pizza_temperature"])
-async def print_text(text)--> DTO_TemperatureRecord:
+#this end point handles a get request.
+@app.get("/pizza/get_temperature", response_model=DTO_TemperatureRecord)
+async def print_text(text) --> DTO_TemperatureRecord:
     """
     docstring: Defining with async in case I have concurrent requests
     Returns: a dictionary 
     """
 
 
-    #data = some transformation of DTO_TemperatureRecord
+    #data = am I getting the data from a database?
+    
     
     try:
         #return the message
